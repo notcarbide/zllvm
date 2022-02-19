3d05914b04bf9eac0b2f6377b2f11b8d1c67d6d3
notcarbide <luis@0k.cx>
2022-02-19 01:05:14 -0500

    ZapDragon: Initial LLVM toolchain with PGO optimizations for 64bit Pi
    Initial scripts to build a PGO clang with basic profiling based on
    cross compilation of arm64 rpi targets. Reduces build time by approx
    25%.
    
    Zapdragon is an x86 toolchain with minimal support for armhf and
    arm64.
    
    NOTE: LTOThin is only selected due to Node- related errors with
          Full LTO.
    
    Signed-off-by: notcarbide <luis@0k.cx>

---
+++ zap/pullsrc.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+BRANCH=release/14.x
+
+reset_repo ()
+{
+  cd $PWD/$1
+  git clean -qfd
+  git checkout .
+  git remote update > /dev/null
+  git reset --hard origin/$BRANCH > /dev/null
+  git clean -qfd
+  git checkout $BRANCH > /dev/null
+  git pull
+}
+
+if [ ! -d llvm-project ]; then
+  echo "Downloading LLVM Project source code..."
+  git clone https://github.com/llvm/llvm-project -b $BRANCH
+else
+  echo "Resetting LLVM Project source code to origin/$BRANCH..."
+  reset_repo llvm-project
+fi
+
+exit
